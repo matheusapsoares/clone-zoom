21b1b5681ae07e4d4a7a57a4d9247abe12d5cd9f
Matheus Soares <matheus.soares@twsolutions.com.br>
2021-01-29 00:37:50 -0300

    Ajustes no design da home

--- public/pages/home/index.js
+++ public/pages/home/index.js
@@ -1,19 +1,54 @@
 window.onload = () => {
     const redirectWithRoomId = element => {
         element.addEventListener('click', () => {
-            const room = prompt('Room name')
+            const room = prompt('Por favor, digite o nome da sala')
             if (!room) {
-                alert('wrong room')
+                alert('Nenhuma sala disponivel')
                 return;
             }
-
             window.open('/pages/room/?room=' + room);
+        })
+    }
+
+    const msgWithout = element => {
+        element.addEventListener('click', () => {
+            alert('Em construção...');
 
         })
     }
+
     const join = document.getElementById('join')
     const newMeeting = document.getElementById('new-meeting')
 
+    const schedule = document.getElementById('schedule')
+    const screen_share = document.getElementById('screen_share')
+
     redirectWithRoomId(join)
     redirectWithRoomId(newMeeting)
+
+    msgWithout(schedule)
+    msgWithout(screen_share);
+
+    /* Seta a data atual */
+    setDateNow();
+
+}
+
+setDateNow = () => {
+    /* adiciona o date */
+    const now = new Date;
+    const time = `${now.getHours()}:${now.getMinutes()}`;
+
+    const clock = document.getElementById('clock')
+
+    /* substitui o valor na view */
+    clock.innerHTML = (time);
+
+    const dayName = ["domingo", "segunda", "terça", "quarta", "quinta", "sexta", "sábado"];
+    const monName = ["janeiro", "fevereiro", "março", "abril", "Maio", "junho", "agosto", "outubro", "novembro", "dezembro"];
+
+    const date = `${dayName[now.getDay()]}, ${now.getDate()} de ${monName[now.getMonth()]} de ${now.getFullYear()}`;
+
+    const dateId = document.getElementById('date')
+    dateId.innerHTML = (date);
 }
